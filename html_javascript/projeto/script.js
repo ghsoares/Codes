5aefb3681e0758a728735cb35872246e7aec3ec0
Dev ifsp ptb <dev.ifsp.ptb@gmail.com>
2019-08-14 10:44:43 -0300

    a

--- html_javascript/projeto/script.js
+++ html_javascript/projeto/script.js
@@ -4,24 +4,34 @@ var collapsables = document.getElementsByClassName("collapse");
 //botões com link
 var button_links = document.getElementsByClassName("link");
 
+//função de redirecionamento
+function redirect(event) {
+    var target = event.path[0];
+    var link = target.getAttribute("data-link");
+    if (link) {
+        window.location.replace(link);
+    }
+}
+
+//função de expansão de uma opção
+function expand(event) {
+    var target = event.path[0].nextElementSibling;
+    if (target.style.height) {
+        target.style.height = null;
+    } else {
+        target.style.height = target.scrollHeight + "px";
+    }
+}
+
 //passa por cada opção colapsável
 for (option of collapsables) {
-    option.addEventListener("click", () => {
-        var target = option.nextElementSibling;
-        if (target.style.height) {
-            target.style.height = null;
-        } else {
-            target.style.height = target.scrollHeight + "px";
-        }
-    })
+    var target = option.nextElementSibling;
+    option.addEventListener("click", expand);
 }
+
+
 
 //passa por cada botão com link
 for (b_link of button_links) {
-    b_link.addEventListener("click", () => {
-        var attribute = b_link.getAttribute("data-link");
-        if (attribute) {
-            window.location.replace(attribute);
-        }
-    })
+    b_link.addEventListener("click", redirect);
 }
