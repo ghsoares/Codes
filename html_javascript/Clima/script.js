e080dfd415aa0405805cf24e5b33a10fa552d85e
Dev ifsp ptb <dev.ifsp.ptb@gmail.com>
2019-08-20 18:22:21 -0300

    a

--- html_javascript/Clima/script.js
+++ html_javascript/Clima/script.js
@@ -1,33 +1,5 @@
-//elemento do botão do menu
-var options_buttons = document.getElementsByClassName("options-button");
+Weather.setApiKey("817edc3a3d0710414e7a6e05d9499bfc")
 
-//menu está aberto
-var menu_open = false;
-
-//passa por cada botão
-for (button of options_buttons) {
-    //novo escutador de evento
-    button.addEventListener('click', (event) => {
-        //alvo
-        let target = $(event.path[0]).children().prevObject[0];
-
-        //alvos
-        let targets = [
-            $(target).find('#b1'),
-            $(target).find('#b2'),
-            $(target).find('#b3')
-        ]
-
-        if (menu_open) {
-            for (bar of targets) {
-                $(bar).css("background-color", "white");
-            }
-        } else {
-            for (bar of targets) {
-                $(bar).css("background-color", "black");
-            }
-        }
-
-        menu_open = !menu_open;
-    })
-}
+Weather.getCurrent("São Paulo", (current) => {
+    console.log(current);
+})
