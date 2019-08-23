9662e6bc9b6a80f29d03af54a1357966b4fcb5cc
gdgamedev <43936806+gdgamedev@users.noreply.github.com>
2019-08-22 22:34:53 -0300

    sa

---
+++ html_javascript/SmarthPhone/js/script.js
@@ -0,0 +1,9 @@
+start();
+
+//inicia com a página
+function start() {
+    console.log("a")
+    //pega todos os botões de aba
+    var tab_buttons = $('.tab-button');
+    console.log(tab_buttons);
+}
