d25446e530d806c40b892462860a9e67e48c0957
gdgamedev <43936806+gdgamedev@users.noreply.github.com>
2019-08-23 22:17:02 -0300

    a

--- html_javascript/SmarthPhone/projects/MeuMelhorAmigo/js/script.js
+++ html_javascript/SmarthPhone/projects/MeuMelhorAmigo/js/script.js
@@ -1,24 +1,3 @@
-//pega todos os botões de aba
-var tab_buttons;
-
 //inicia com a página
 function start() {
-    tab_buttons = $(".tab-button");
-    //passa por cada botão
-    for (tab_button of tab_buttons) {
-        //ação de pressionar o botão
-        tab_button.addEventListener("click", function(event) {
-            //pega o botão atual
-            var active = $(".active")[0];
-
-            //muda a classe
-            active.classList.remove("active");
-
-            //alvo
-            var target = event.target;
-
-            //muda a classe do alvo
-            target.classList.add("active");
-        });
-    }
 }
