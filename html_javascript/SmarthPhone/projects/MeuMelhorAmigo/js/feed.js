841a1042c2755ca537c74020c85edc9dfa86ffb3
gdgamedev <43936806+gdgamedev@users.noreply.github.com>
2019-08-25 14:54:14 -0300

    a

--- html_javascript/SmarthPhone/projects/MeuMelhorAmigo/js/feed.js
+++ html_javascript/SmarthPhone/projects/MeuMelhorAmigo/js/feed.js
@@ -12,3 +12,23 @@ function newPost() {
     //classe do elemento
     post.className = "post";
 }
+
+//quando tenta ver mais da descrição de um post
+function readMore(btn) {
+    //caminho do span dos três pontos (...)
+    var dots = $(btn).prev().find(".dots")[0];
+
+    //caminho do resto do texto
+    var more = $(btn).prev().find(".more")[0];
+
+    //o texto está escondido
+    if (dots.style.display == "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "READ MORE";
+        more.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btn.innerHTML = "READ LESS";
+        more.style.display = "inline";
+    }
+}
